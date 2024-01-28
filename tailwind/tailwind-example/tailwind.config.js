cfcd11b24868881370d49b71f9372b12b6b1d861
asdf1596 <7082mc@naver.com>
2024-01-28 20:04:55 +0900

    tailwind restart

--- tailwind/tailwind-example/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["tailwind/tailwind-example/src/App.js"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
