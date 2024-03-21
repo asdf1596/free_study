9619825a0bfea016d14efb5c4f971b3d88575aca
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-03-21 23:53:58 +0900

    0321-23:53

---
+++ test/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <body className="m-0 p-10 grid justify-items-center">
+            <div className="bg-[#ACD1DC] m-0 w-1/2 grid justify-items-center">
+                <div className="bg-[#000000] text-white">fdsa</div>
+                <div className="bg-[#000000] text-white">fdsa</div>
+            </div>
+        </body>
+    );
+}
